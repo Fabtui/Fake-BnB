12f51466162f023eb995a8a6ee78c54ad598c3c1
Fab <fabrice.tuitui@gmail.com>
2021-12-14 00:10:38 +0100

    add repo

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,12 +1,26 @@
 <?php 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class PropertyController extends AbstractController
 {
+
+  /**
+   * @var PropertyRepository
+   */
+  private $repository;
+
+  public function __construct(PropertyRepository $repository)
+  {
+    $this->repository = $repository;
+  }
+
   /**
    * @Route("/features", name="property.index")
    * @return Reponse
@@ -14,6 +28,8 @@ class PropertyController extends AbstractController
 
   public function index(): Response
   {
+    // $property = $this->repository->findOneBy(['floor' => 4]);
+    // dump($property);
     return $this->render('property/index.html.twig' ,[
       'current_menu' => 'properties'
     ]);
